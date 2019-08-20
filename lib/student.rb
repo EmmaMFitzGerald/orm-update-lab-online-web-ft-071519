82edb66c2f5bb166f24365299fb84f28a3f9d29c
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-20 12:50:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,14 @@ class Student
       student.save
       student
     end
+    
+  def self.new_from_db(row)
+    new_song = self.new  # self.new is the same as running Song.new
+    new_song.id = row[0]
+    new_song.name =  row[1]
+    new_song.length = row[2]
+    new_song  # return the newly created instance
+  end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
